7889f5f6640129d97bf4226489a17ec4b3905881
mbacked123 <abdoul.diouf@gmail.com>
2016-03-18 12:45:25 +0000

    mis à jour des utilitaires

--- ModuleFunc.py
+++ ModuleFunc.py
@@ -1,7 +1,8 @@
+# -*- coding: utf-8 -*-
 def LongestWord(sen): 
+  """une fonction retournant le plus long mot dans une phrase"""
 
-  # code goes here 
-  # Note: don't forget to properly indent in Python
+  
   def operation_taille(val):
     taille = len(val)
     for c in val:
@@ -19,3 +20,40 @@ def LongestWord(sen):
       dico = value
   
   return dico, maxi 
+
+
+  def FirstFactorial(num): 
+    """Fonction calculant le factoriel d'un nombre données"""
+
+
+  if num == 0 or num == 1:
+    return 1
+  else:
+    num = num * FirstFactorial(num-1)    
+  return num    
+
+
+def LetterChanges(str): 
+  """fonction changeant chaque lettre par sa suivante en mettant les voyelles en majuscule"""
+
+ 
+  alphabet = ['a','b','c','d','e','f','g','h','i','j','i','k','l',
+              'm','n','o','p','q','r','s','t','u','v','w','x','y','z']
+  vowel = ['a','e','i','o','u']
+  chaine = ""
+  for i in str:
+    if i in alphabet:
+      ind = alphabet.index(i)
+      if ind == (len(alphabet) -1):
+        chaine += alphabet[0].upper()
+      else:
+        if alphabet[ind + 1] in vowel:
+          chaine += alphabet[ind + 1].upper()
+        else:
+          chaine += alphabet[ind + 1]
+    else:
+      chaine += i
+     
+    str = chaine
+    
+  return str    
